0ea5d360a93e3fb3b8a8b59b547d561129ace187
Bryan Pibaque <bryan-alexiss1@hotmail.com>
2022-09-16 18:47:59 +0200

    Add project files.

---
+++ ProyectoEFE/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoEFE.Startup))]
+namespace ProyectoEFE
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
