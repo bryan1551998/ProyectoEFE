7b6ebe6d4519ef94d6a2cf3745abec83ac008e64
Sebastian  Contreras <lcontrerassebastian@gmail.com>
2022-09-19 14:27:26 +0200

    sa

--- ProyectoEFE/Models/LessonsModel.cs
+++ ProyectoEFE/Models/LessonsModel.cs
@@ -7,5 +7,15 @@ namespace ProyectoEFE.Models
 {
     public class LessonsModel
     {
+        string name_curs;
+        string description_curs;
+        string image_url_curs;
+        public LessonsModel(string nc, string dc, string iuc)
+        {
+            name_curs = nc;
+            description_curs = dc;
+            image_url_curs = iuc;
+        }
+
     }
 }
