a43c25e091bca3572162b7b084ac0dddc9ecd47c
Sebastian  Contreras <lcontrerassebastian@gmail.com>
2022-09-19 11:00:58 +0200

    DAl and models

---
+++ ProyectoEFE/DAL/DalCurs.cs
@@ -0,0 +1,39 @@
+﻿using ProyectoEFE.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoEFE.DAL
+{
+    public class DalCurs
+    {
+      string strconx = ConfigurationManager.ConnectionStrings["con"].ConnectionString ;
+        public void AddCurs(CursModel cursm)
+        {
+            SqlConnection con = new SqlConnection(strconx);
+            con.Open();
+
+            string cadena = "insert into curs(name_curs, description_curs, icon)" +
+                " values ('" + cursm.Name_curs + "'" +
+                ",'" + cursm.Description_curs + "','" + cursm.Image_url_curs + "')";
+
+            try
+            {
+                SqlCommand comando = new SqlCommand(cadena, con);
+                comando.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                con.Close();
+            }
+        }
+
+        internal void AddCurs()
+        {
+            throw new NotImplementedException();
+        }
+    }
--- ProyectoEFE/Modelos/CursModel.cs
+++ ProyectoEFE/Modelos/CursModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ProyectoEFE.Modelos
+﻿namespace ProyectoEFE.Modelos
 {
     public class CursModel
     {
-        public int prueba;
+        string name_curs;
+        string description_curs;
+        string image_url_curs;
+        public CursModel(string nc, string dc, string iuc)
+        {
+            name_curs = nc;
+            description_curs = dc;
+            image_url_curs = iuc;
+        }
+
+        public string Name_curs { get => name_curs; set => name_curs = value; }
+        public string Description_curs { get => description_curs; set => description_curs = value; }
+        public string Image_url_curs { get => image_url_curs; set => image_url_curs = value; }
     }
 }
