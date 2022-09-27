57ebd7759043b7eab790b15802f9936f0bf48329
Sebastian  Contreras <lcontrerassebastian@gmail.com>
2022-09-27 17:33:33 +0200

    user
    Users

---
+++ ProyectoEFE/DAL/DALUser.cs
@@ -0,0 +1,54 @@
+﻿using ProyectoEFE.Conexion;
+using ProyectoEFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoEFE.DAL
+{
+    public class DALUser
+    {
+        public List<UserModel> SelectUser(string email)
+        {
+            ConexionBD cnn = new ConexionBD();
+            List<UserModel> lisModels = new List<UserModel>();
+
+            try
+            {
+                //String query
+                String query =@"SELECT FirstName, LastName, BirthDay FROM AspNetUsers WHERE Email='" + email + "'";
+
+                //Conexion creada
+                SqlCommand comand = new SqlCommand(query, cnn.Connection);
+
+                //Ejecutar query
+                SqlDataReader registros = comand.ExecuteReader();
+
+                //Obtener lo datos
+                while (registros.Read())
+                {
+                    UserModel userModel = new UserModel();
+                    userModel.FirstName1 = (String)registros["FirstName"];
+                    userModel.LastName1 = (String)registros["LastName"];
+                    userModel.BirthDay1 = (DateTime)registros["BirthDay"];
+                    userModel.NickName1 = (String)registros["NickName"];
+                    userModel.ProfilePicture1 = (String)registros["ProfilePicture"];
+                    lisModels.Add(userModel);
+                }
+            }
+            catch (Exception exeption)
+            {
+                Debug.WriteLine("ERROR SELECT USER: " + exeption.Message);
+            }
+            finally
+            {
+                cnn.CerrarConexion();
+            }
+
+            return lisModels;
+            }
+    }
+}
---
+++ ProyectoEFE/Models/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoEFE.Models
+{
+    public class UserModel
+    {
+        //Atributos
+        string NickName;
+        string FirstName;
+        string LastName;
+        string ProfilePicture;
+        DateTime BirthDay;
+
+        //Get and Set
+        public string NickName1 { get => NickName; set => NickName = value; }
+        public string FirstName1 { get => FirstName; set => FirstName = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+        public DateTime BirthDay1 { get => BirthDay; set => BirthDay = value; }
+        public string ProfilePicture1 { get => ProfilePicture; set => ProfilePicture = value; }
+
+        //Constructors
+        public UserModel(string nickName, string firstName, string lastName, DateTime birthDay, string profilePicture)
+        {
+            this.NickName = nickName;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDay = birthDay;
+            this.ProfilePicture = profilePicture;
+        }
+        public UserModel()
+        {
+
+        }
+    }
+}
