ef80f34658abe81978c87b0389ea6830c9ae839f
Bryan Pibaque <bryan-alexiss1@hotmail.com>
2022-09-27 23:15:37 +0200

    Modificación ViewSwitcher

--- ProyectoEFE/ViewSwitcher.ascx.cs
+++ ProyectoEFE/ViewSwitcher.ascx.cs
@@ -21,10 +21,10 @@ namespace ProyectoEFE
         {
             // Determine current view
             var isMobile = WebFormsFriendlyUrlResolver.IsMobileView(new HttpContextWrapper(Context));
-            CurrentView = isMobile ? "Mobile" : "Desktop";
+            CurrentView = "Desktop";
 
             // Determine alternate view
-            AlternateView = isMobile ? "Desktop" : "Mobile";
+            AlternateView = "Desktop";
 
             // Create switch URL from the route, e.g. ~/__FriendlyUrls_SwitchView/Mobile?ReturnUrl=/Page
             var switchViewRouteName = "AspNet.FriendlyUrls.SwitchView";
