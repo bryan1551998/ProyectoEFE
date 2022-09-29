33c94432971f276b8bfaf8d521ad606577aa88c1
Bryan Pibaque <bryan-alexiss1@hotmail.com>
2022-09-29 22:00:42 +0200

    DALCursoUser

---
+++ ProyectoEFE/DAL/DALCursoUser.cs
@@ -0,0 +1,61 @@
+﻿using ProyectoEFE.Conexion;
+using ProyectoEFE.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoEFE.DAL
+{
+    public class DALCursoUser
+    {
+        public void InsertCursUser(String id_user, int curs) {
+
+
+            ConexionBD cnn = new ConexionBD();
+
+            try
+            {
+                //String query
+                String query = @"INSERT INTO relationship_curs_user 
+                                VALUES(@pFk_curs,@pFk_users,@pFk_exercise);
+";
+
+                //Conexion creada
+                SqlCommand comand = new SqlCommand(query, cnn.Connection);
+
+                //Parametros de la query
+                SqlParameter pFk_curs = new SqlParameter("@pFk_curs", );
+                SqlParameter pFk_users = new SqlParameter("@pFk_users", curs.Description_curs);
+                SqlParameter pFk_exercise = new SqlParameter("@pFk_exercise", curs.Image_url_curs);
+
+                //Añadir los parametros
+                comand.Parameters.Add(pName_curs);
+                comand.Parameters.Add(pDescription_curs);
+                comand.Parameters.Add(pImage_curs);
+                comand.Parameters.Add(pAutor_name);
+                comand.Parameters.Add(pAutor_idUser);
+
+                //Ejecutar query
+                comand.ExecuteNonQuery();
+                Debug.WriteLine("Curso " + curs.Name_curs + " creado");
+
+            }
+            catch (Exception exeption)
+            {
+                Debug.WriteLine("ERROR INSERTAR CURS: " + exeption.Message + " --- " + curs.Name_curs);
+            }
+            finally
+            {
+                cnn.CerrarConexion();
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ ProyectoEFE/Models/CursUserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoEFE.Models
+{
+    public class CursUserModel
+    {
+        //Parametros
+        int fk_curs;
+        String fk_users;
+        int? fk_exercise;
+
+        //GET and SET
+        public int Fk_curs { get => fk_curs; set => fk_curs = value; }
+        public string Fk_users { get => fk_users; set => fk_users = value; }
+        public int? Fk_exercise { get => fk_exercise; set => fk_exercise = value; }
+
+        //Constructor
+        public CursUserModel(int fk_curs, string fk_users, int fk_ecercise)
+        {
+            this.Fk_curs = fk_curs;
+            this.Fk_users = fk_users;
+            this.Fk_ecercise = fk_ecercise;
+        }
+
+        public CursUserModel()
+        {
+        }
+    }
+}
